be2bcae48f7ac066ac3075007a710b4b8839cf04
Benjamin Fry <benjaminfry@me.com>
2023-12-08 11:16:59 -0800

    add test for Welcome response

--- src/response.rs
+++ src/response.rs
@@ -4,3 +4,15 @@ use serde::Deserialize;
 pub struct WelcomeResponse {
     pub msg: String,
 }
+
+#[test]
+fn test_deserialize_welcome() {
+    let json = r#"
+    {
+        "msg": "hello! Telraam server 2.0 is up and running"
+    }
+    "#;
+
+    let welcome = serde_json::from_str::<WelcomeResponse>(json).expect("failed to parse json");
+    assert_eq!("hello! Telraam server 2.0 is up and running", welcome.msg);
+}
